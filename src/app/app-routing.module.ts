324565f17d9660437220950d279f3e4f33627e9d
Dubiel Santana <dubiels86@gmail.com>
2021-08-27 15:51:41 -0400

    todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
       { path: "dashboard", component: DashboardComponent},
       { path: "todo", component: GroupComponent},
      
-      { path: "", redirectTo: "/admin/dashboard", pathMatch: "full" },
+      { path: "", redirectTo: "todo", pathMatch: "full" },
     ],
   },
   
